0af574067552618254fba2fa3be6df5a7c5d3d1e
Christian Wollny <chwo.dev@gmail.com>
2021-08-03 16:52:27 +0200

    add sample app project

---
+++ SampleApp/SampleAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SampleAppApp.swift
+//  SampleApp
+//
+//  Created by Christian Wollny on 03.08.21.
+//
+
+import SwiftUI
+
+@main
+struct SampleAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
