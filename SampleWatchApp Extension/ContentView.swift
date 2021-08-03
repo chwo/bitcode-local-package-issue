f162c278efd581710eb3a56387e5380c4853b13c
Christian Wollny <chwo.dev@gmail.com>
2021-08-03 16:53:21 +0200

    add sample watch app

---
+++ SampleWatchApp Extension/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SampleWatchApp Extension
+//
+//  Created by Christian Wollny on 03.08.21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SampleWatchApp Extension/SampleAppApp.swift
@@ -0,0 +1,19 @@
+//
+//  SampleAppApp.swift
+//  SampleWatchApp Extension
+//
+//  Created by Christian Wollny on 03.08.21.
+//
+
+import SwiftUI
+
+@main
+struct SampleAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            NavigationView {
+                ContentView()
+            }
+        }
+    }
+}
